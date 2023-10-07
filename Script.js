1bc417778c0e5128f2953bfc8c421fb4bf3d5389
Prathmesh Gorle <86977369+Prathmesh-Gorle@users.noreply.github.com>
2023-10-07 16:46:48 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,13 @@
+let z=document.getElementById('body1')
+function colorcg(){
+    z.style.background="blue"
+    
+}
+
+let x=document.getElementById('h1')
+console.log(x)
+function changetxt(){
+    x.textContent='You have Succsefully Manupulated'
+    x.style.fontSize = 10
+}
+
